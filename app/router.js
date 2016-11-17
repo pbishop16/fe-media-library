c6793dcb377b5ac5ebde9e81eabb0ddc679c6955
Paul Bishop <pbishop16@gmail.com>
2016-11-17 13:39:56 -0600

    added basic album routing

--- app/router.js
+++ app/router.js
@@ -9,6 +9,9 @@ Router.map(function() {
   this.route('artists', function() {
     this.route('artist', {path: ':artist_id'});
   });
+  this.route('albums', function() {
+    this.route('album', {path: ':album_id'});
+  });
 });
 
 export default Router;
