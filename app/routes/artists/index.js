f45a025ac54b5e003f2853147bfef0e7f0f1974f
Paul Bishop <pbishop16@gmail.com>
2016-11-17 16:13:44 -0600

    basic clean up

--- app/routes/artists/index.js
+++ app/routes/artists/index.js
@@ -15,7 +15,7 @@ export default Ember.Route.extend({
       page: params.page
     });
   },
-  resetController(controller, isExiting, transition) {
+  resetController(controller, isExiting) {
     if (isExiting) {
       controller.setProperties({locationFilter: '',sort_on: 'name', sort_direction: '', per_page: 0, page: 0});
     }
