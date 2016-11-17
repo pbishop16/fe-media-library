69fa2a83416848e4bf04b519606921a5be27e2ae
Paul Bishop <pbishop16@gmail.com>
2016-11-17 16:05:41 -0600

    updated model for filtering

--- app/models/artist.js
+++ app/models/artist.js
@@ -8,5 +8,7 @@ export default DS.Model.extend({
   founding_year: attr(),
   album_ids: hasMany('album'),
   created_at: attr('Date'),
-  updated_at: attr('Date')
+  updated_at: attr('Date'),
+  allAlbumSizes: Ember.computed.mapBy('album_ids', 'total_sold'),
+  totalAlbumsSold: Ember.computed.sum('allAlbumSizes')
 });
