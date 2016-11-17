92c492380b902fe24e568951396e17d1e10a73d9
Paul Bishop <pbishop16@gmail.com>
2016-11-17 16:06:45 -0600

    application index filtering implementation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  metaValues: Ember.inject.service('meta-values'),
+  queryParams: {
+    artists_per_page: { refreshModel: true },
+    albums_per_page: { refreshModel: true },
+    sort_on: { refreshModel: true }
+  },
+  model() {
+    return this.get('metaValues').getArtistMeta().then(() => {
+      return this.get('metaValues').getAlbumMeta().then(() => {
+        return Ember.RSVP.hash({
+          artists: this.store.query('artist', {per_page: this.get('metaValues').artistMeta.total}),
+          albums: this.store.query('album', {sort_on: 'year', per_page: this.get('metaValues').albumMeta.total})
+        });
+      });
+    });
+  }
 });
